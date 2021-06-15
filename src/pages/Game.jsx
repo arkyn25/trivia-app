50a8db227a26dacd355c17c9058b711a5f593b77
esionascimento <esio.392@protonmail.com>
2021-06-15 14:44:54 -0400

    concluido req 4

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -1,26 +1,35 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-/* import md5 from 'crypto-js/md5';
-import hash from '../components/hash'; */
 
 class Game extends Component {
   render() {
-    const { getEmail } = this.props;
+    const { getNome, getMd5 } = this.props;
+    const url = `https://www.gravatar.com/avatar/${getMd5}`;
     return (
-      <div>
-        { getEmail }
-      </div>
+      <header>
+        <img
+          data-testid="header-profile-picture"
+          src={ url }
+          alt="ls"
+        />
+        <p data-testid="header-player-name">
+          { getNome }
+        </p>
+        <p data-testid="header-score">0</p>
+      </header>
     );
   }
 }
 
 Game.propTypes = {
-  getEmail: PropTypes.string.isRequired,
+  getNome: PropTypes.string.isRequired,
+  getMd5: PropTypes.string.isRequired,
 };
 
 const mapStateToProps = (state) => ({
-  getEmail: state.loginReducer.email,
+  getNome: state.loginReducer.nome,
+  getMd5: state.loginReducer.md5,
 });
 
 export default connect(mapStateToProps)(Game);
--- src/reducers/loginReducer.js
+++ src/reducers/loginReducer.js
@@ -1,8 +1,10 @@
-import { REQUEST_TOKEN, REQUEST_EMAIL } from '../actions';
+import { REQUEST_TOKEN, REQUEST_EMAIL, REQUEST_MD5, REQUEST_NOME } from '../actions';
 
 const INITIAL_STATE = {
   token: '',
   email: '',
+  md5: '',
+  nome: '',
 };
 
 const loginReducer = (state = INITIAL_STATE, action) => {
@@ -16,6 +18,16 @@ const loginReducer = (state = INITIAL_STATE, action) => {
     return {
       ...state,
       email: action.email,
+    };
+  case REQUEST_MD5:
+    return {
+      ...state,
+      md5: action.md5,
+    };
+  case REQUEST_NOME:
+    return {
+      ...state,
+      nome: action.nome,
     };
   default:
     return state;
