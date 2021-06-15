2b8a5ade07f15eec13e463422542b3a999841fa3
legalvao1 <legalvao1@gmail.com>
2021-06-15 15:33:45 -0300

    Salvando as perguntas retornadas da API no redux

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,10 @@
 export const REQUEST_TOKEN = 'REQUEST_TOKEN';
+export const REQUEST_QUESTIONS = 'REQUEST_QUESTIONS';
 
 export const loginAction = (token) => ({
   type: REQUEST_TOKEN, token,
+});
+
+export const questionAction = (questions) => ({
+  type: REQUEST_QUESTIONS, questions,
 });
--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { fetchQuestion } from '../services';
+import { questionAction } from '../actions';
 
 class Questions extends Component {
   async componentDidMount() {
-    const { token } = this.props;
+    const { token, dispatchQuestions } = this.props;
     const response = await fetchQuestion(token);
     const questions = response.results;
-    console.log(questions);
+    dispatchQuestions(questions);
   }
 
   render() {
@@ -24,8 +25,13 @@ const mapStateToProps = (state) => ({
   token: state.loginReducer.token,
 });
 
+const mapDispatchToProps = (dispatch) => ({
+  dispatchQuestions: (questions) => dispatch(questionAction(questions)),
+});
+
 Questions.propTypes = {
   token: PropTypes.string.isRequired,
+  dispatchQuestions: PropTypes.func.isRequired,
 };
 
-export default connect(mapStateToProps, null)(Questions);
+export default connect(mapStateToProps, mapDispatchToProps)(Questions);
